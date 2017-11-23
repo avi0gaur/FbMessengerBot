374f90cb3c2bc771d051d189ddce343d3e7c0fb5
Avinash Gaur <avinash.gaur@crmnext.in>
2017-11-23 18:22:34 +0530

    Added crud

--- user_data.py
+++ user_data.py
@@ -5,9 +5,9 @@ class UserStateData():
     def __init__(self):
         self.data = { "id": "", "intent_type": "", "user_text": "", 'user_stage': 0 }
         self.db = state_mdb()
-        self.updatedb(self.data)
+        self.updatedb()
 
 
-    def updatedb(self, data):
-        self.db.update_user_stage(self.data)
+    def updatedb(self):
+        self.db.insert_user_state(self.data)
 
