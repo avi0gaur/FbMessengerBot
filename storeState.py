18ba03d0af16bd962dadefb8f45298ea64240853
Avinash Gaur <avinash.gaur@crmnext.in>
2017-11-23 18:16:17 +0530

    update

--- storeState.py
+++ storeState.py
@@ -55,7 +55,7 @@ class state_mdb:
         :return:
         """
         try:
-          cr = self.db.user_stage.distinct(id)
+          cr = self.db.user_stage.find({"id":id})
         except Exception as e:
             log.log_error(e)
         return cr
