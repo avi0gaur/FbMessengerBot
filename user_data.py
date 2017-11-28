b167e2d686d9fcae6105720593f280a75b520c6e
Avinash Gaur <avinash.gaur@crmnext.in>
2017-11-28 12:41:00 +0530

    updated skills

--- user_data.py
+++ user_data.py
@@ -3,7 +3,7 @@ from storeState import state_mdb
 class UserStateData():
 
     def __init__(self):
-        self.data = { "id": "", "intent_type": "", "user_text": "", 'user_stage': 0 }
+        self.data = {"id": "", "intent_type": "", "user_text": "", 'user_stage': 0, "card_type":""}
         self.db = state_mdb()
         self.updatedb()
 
