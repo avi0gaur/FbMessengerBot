ae81d3e2e85cd2ed4b722abf98610afc15e6a962
Avinash Gaur <avinash.gaur@crmnext.in>
2017-12-01 13:11:44 +0530

    mod

--- skills_api.py
+++ skills_api.py
@@ -1,5 +1,5 @@
 def authenticate_user(condition):
-    if condition['card_type'] == '#authentication':
+    if condition["card_type"] == '#authentication':
         return "submitted"
     else:
         return condition["response"]
