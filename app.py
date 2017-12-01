f743da7b8f4067c76e0113c8656de4ecb52d131d
Avinash Gaur <avinash.gaur@crmnext.in>
2017-12-01 13:02:12 +0530

    mod

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def fb_webhook():
                         else:
                             u_state = u_data["0"]
 
-                        u_state["user_text"] = authenticate_user(message_text)
+                        u_state["user_text"] = authenticate_user(u_data)
                         res = bot.run_bot(u_state)
                         upd_state(sender_id, res, u_data)
                         send_message(sender_id, res['response_text'])
